bb758e20a640ca4a6c1d8c21258c68c0bbef56c1
yunpeng.liu <allenlyp1992@gmail.com>
2021-10-11 13:48:46 +0100

    fix bug when change sass to less

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,12 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const commonConfig = require('./webpack.common');
+// const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+
+// const smp = new SpeedMeasurePlugin();
+
 
 module.exports = merge(commonConfig, {
   devtool: 'eval-source-map',
@@ -14,6 +20,35 @@ module.exports = merge(commonConfig, {
     ]
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                autoprefixer
+              ]
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              strictMath: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
   output: {
     filename: 'js/[name].js',
     chunkFilename: '[id].chunk.js'
